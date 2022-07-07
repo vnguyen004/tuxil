0ab4bf478e2a54cac7dfd3e37d089379e59e1032
Vien Nguyen <vnguyen@trismax.com>
2022-07-07 07:53:14 -0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,42 @@
 
 namespace Database\Seeders;
 
+use App\Models\Skill;
+use App\Models\User;
+use Carbon\Carbon;
+use Faker\Generator;
+use Illuminate\Container\Container;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * The current Faker instance.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     * Create a new seeder instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->faker = $this->withFaker();
+    }
+
+    /**
+     * Get a new Faker instance.
+     *
+     * @return \Faker\Generator
+     */
+    protected function withFaker()
+    {
+        return Container::getInstance()->make(Generator::class);
+    }
+
     /**
      * Seed the application's database.
      *
@@ -13,6 +45,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Vien Nguyen',
+            'username' => 'admin',
+            'email' => 'vien@gmail.com',
+            'is_public' => true,
+            'is_admin' => true,
+            'password' => 'admin!',
+        ]);
+
+        // create 50 users
+        User::factory(50)->create();
+
+        // create 10 skills
+        Skill::factory(10)->create();
+
+        // randomly assign skills to all users
+        collect(User::all())->map(function (User $user) {
+            collect(Skill::all()->random(3))->map(function (Skill $skill) use ($user) {
+                $user->skills()->attach($skill, [
+                    'desc' => $this->faker->sentence(),
+                    'since' => (Carbon::now())->subYears(rand(1, 10)),
+                ]);
+            });
+        });
     }
 }
