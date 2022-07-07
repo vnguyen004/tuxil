d644b2eba9564d0b2007a1852b81b4ca7e14a30d
Vien Nguyen <vnguyen@trismax.com>
2022-07-07 07:53:29 -0700

    update migrations

---
+++ database/migrations/2022_07_07_085813_create_user_skills_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_skills', function (Blueprint $table) {
+            $table->primary(['user_id', 'skill_id']);
+            $table->text('desc');
+            $table->date('since')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('skill_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_skills');
+    }
+};
